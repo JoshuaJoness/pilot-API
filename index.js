8c687f4cac3cc431717ca85553bda56c0844889c
Joshua Jones <joshuajones@trentu.ca>
2020-01-17 19:28:08 -0500

    set up pusher

--- index.js
+++ index.js
@@ -29,3 +29,26 @@ app.listen(4000, () => {
 })
 
 app.post('/booking', require('./controllers/postBooking.js'))
+
+app.post('/pusher/auth', (req, res) => {
+		let socketId = req.body.socket_id;
+		let channel = req.body.channel_name;
+		random_string = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+		let presenceData = {
+				user_id: random_string,
+				user_info: {
+						username: '@' + random_string,
+				}
+		};
+		let auth = pusher.authenticate(socketId, channel, presenceData);
+		res.send(auth);
+});
+
+app.post('/update-location', (req, res) => {
+		// trigger a new post event via pusher
+		pusher.trigger('presence-channel', 'location-update', {
+				'username': req.body.username,
+				'location': req.body.location
+		})
+		res.json({ 'status': 200 });
+});
